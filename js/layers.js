d384a3ac314c9b905d4ac4073de9b557a35f6b25
AngryStar6K <153428618+AngryStar6K@users.noreply.github.com>
2025-06-30 16:14:03 +0800

    v0.6 hotfix1

--- js/layers.js
+++ js/layers.js
@@ -15961,7 +15961,7 @@ addLayer("crafting_table", {
                 需要材料：1黄铜齿轮 + 200石头 + 200木头<br>
                 复杂度：${formatWhole(this.complexity)}<br>
                 倍率：${fw(this.mult())}<br>
-                数量：${fw(player[ct].items[281])} ${hasUpgrade(steel, 11) ? `(${fw(this.mult())}/s)` : ""}<br>
+                数量：${fw(player[ct].items[282])} ${hasUpgrade(steel, 11) ? `(${fw(this.mult())}/s)` : ""}<br>
                 效果：需要合成应力表后才可发挥用途`
                 return d
             },
@@ -15973,7 +15973,7 @@ addLayer("crafting_table", {
             },
             effectiveMult() {
                 let m = this.mult()
-                m = m.min(player.crafting_table.items[282]).min(player.wood.points.div(200).floor()).min(player.stone.points.div(200).floor())
+                m = m.min(player.crafting_table.items[281]).min(player.wood.points.div(200).floor()).min(player.stone.points.div(200).floor())
                 return m
             },
             autoCraft(diff) {
