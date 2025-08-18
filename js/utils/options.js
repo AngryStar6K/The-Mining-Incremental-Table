6da6d98fcd5db15b0c89b0e96b9cb63188189757
AngryStar6K <153428618+AngryStar6K@users.noreply.github.com>
2025-08-19 03:01:49 +0800

    25w34a

--- js/utils/options.js
+++ js/utils/options.js
@@ -149,10 +149,10 @@ function switchDefaultUpgSize() {
 	options.biggerUpgs = !options.biggerUpgs
 }
 
-var notations = ['Scientific', 'Hyper-E', 'Letter', 'Emoji'] //想在ExpantaNum.js里搓记数法还是很吃力的（
-var notationsZH = ['科学(FGH-JK)', '超-E', '字母', 'Emoji']
-function changeNotation() {//记数法没有修好
-	//options.notation = notations[(notations.indexOf(options.notation) + 1) % notations.length]
+var notations = ['Scientific', 'Hyper-E', 'Letter', 'Emoji', 'Chinese'] //想在ExpantaNum.js里搓记数法还是很吃力的（
+var notationsZH = ['科学(FGH-JK)', '超-E', '字母', 'Emoji', '中文']
+function changeNotation() {//记数法没有修好 -- 25w34a进入测试状态
+	options.notation = notations[(notations.indexOf(options.notation) + 1) % notations.length]
 }
 
 function setUpdatingRate() {
